e6d46f66ccc077b15c07f704d4cbe6b5f426cd05
alessandra2016 <alehvital@gmail.com>
2016-06-11 02:00:05 -0300

    Parte do agendamento - alterações4

--- src/pi/dao/clienteDao.java
+++ src/pi/dao/clienteDao.java
@@ -32,57 +32,102 @@ public class clienteDao {
         } catch (SQLException e) {
             Conexao.imprimeErro("Erro ao apagar cliente", e.getMessage());
         } finally {
-            Conexao.fechar(this.comando);
+            //Conexao.fechar(this.comando);
         }
     }
 
-
-//atualizar os dados do banco tabela cliente
-    public void atualizar(cliente cliente) throws SQLException {
+    //atualizar os dados do banco tabela cliente
+    public void atualizar(cliente cliente, int n, String dados) throws SQLException {
         Statement comando1 = this.comando.createStatement();
-        String com = "UPDATE CLIENTE SET NOME = '" + cliente.getNome()
-                + "', CPF =" + cliente.getCpf() + "', RG =" + cliente.getRg()+ ", TELEFONE = '"
-                + cliente.getTelefone() + "', CELULAR ='" + cliente.getCelular()
-                + "' WHERE  ID_CLIENTE = '" + cliente.getIdcliente()+ "';";
-        System.out.println("Atualizada!");
+        cliente clie = new cliente();
         try {
-            comando1.executeUpdate(com);
+
+            if (n == 1) {
+                String query = "UPDATE CLIENTE SET NOME_CLIENTE = '" + cliente.getNome() + "', NOME_CLIENTE = '" + dados+ "' where ID_CLIENTE = '" + cliente.getIdcliente() + "';";
+
+                System.out.println(" Nome Atualizado!");
+                comando1.executeUpdate(query);
+            } else if (n == 2) {
+
+                String query = "UPDATE CLIENTE SET CPF = '" + cliente.getCpf() + "', CPF = '" + cliente.getCpfNovo() + "' where ID_CLIENTE = '" + cliente.getIdcliente() + "';";
+
+                System.out.println(" CPF Atualizado!");
+                comando1.executeUpdate(query);
+            } else if (n == 3) {
+
+                String query = "UPDATE CLIENTE SET RG = '" + cliente.getRg() + "', RG = '" + cliente.getRgNovo() + "' where ID_CLIENTE = '" + cliente.getIdcliente() + "';";
+
+                System.out.println(" RG Atualizado!");
+                comando1.executeUpdate(query);
+            } else if (n == 4) {
+                String query = "UPDATE CLIENTE SET TELEFONE = '" + cliente.getTelefone()+ "', TELEFONE = '" + cliente.getTelefoneNovo()+ "' where ID_CLIENTE = '" + cliente.getIdcliente() + "';";
+
+                System.out.println(" Telefone Atualizado!");
+                comando1.executeUpdate(query);
+            } else if (n == 5) {
+                String query = "UPDATE CLIENTE SET CELULAR = '" + cliente.getCelular()+ "', CELULAR = '" + cliente.getCelularNovo()+ "' where ID_CLIENTE = '" + cliente.getIdcliente() + "';";
+
+                System.out.println(" Celular Atualizado!");
+                comando1.executeUpdate(query);
+            }
+
+            //com é a mesma variavel que esta guardando a String
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            Conexao.fechar(this.comando);
+            Conexao.imprimeErro("Erro ao atualizar cliente", e.getMessage());
         }
     }
-    
-    
-
-    
 
     public void buscar(cliente cliente) throws SQLException {
         Statement comando1 = this.comando.createStatement();
-        
+
         ResultSet rs;
         try {
             rs = comando1.executeQuery("SELECT * FROM CLIENTE");
             while (rs.next()) {
                 cliente temp = new cliente();
                 // pega todos os atributos da pessoa e guarda na classe
-                temp.setNome(rs.getString("ID_CLIENTE"));
+                temp.setIdcliente(rs.getInt("ID_CLIENTE"));
                 temp.setNome(rs.getString("NOME_CLIENTE"));
                 temp.setCpf(rs.getString("CPF"));
                 temp.setRg(rs.getString("RG"));
                 temp.setTelefone(rs.getString("TELEFONE"));
                 temp.setCelular(rs.getString("CELULAR"));
-               
+
                 // exibe os cadastros no banco
-               
-                System.out.println(rs.getString("ID_CLIENTE") +" | "+rs.getString("NOME_CLIENTE") + " | " + rs.getString("CPF") + " | " +rs.getString("RG") + " | " + rs.getString("TELEFONE") + " | " +rs.getString("CELULAR"));
-           
+                System.out.println(rs.getInt("ID_CLIENTE") + " | " + rs.getString("NOME_CLIENTE") + " | " + rs.getString("CPF") + " | " + rs.getString("RG") + " | " + rs.getString("TELEFONE") + " | " + rs.getString("CELULAR"));
+
             }
         } catch (SQLException e) {
             Conexao.imprimeErro("Erro ao buscar cliente", e.getMessage());
         }
 
+    }
+
+    public cliente buscarEspecifica(int idCliente) throws SQLException {
+        Statement comando1 = this.comando.createStatement();
+        cliente temp = new cliente();
+        String query = "SELECT * FROM CLIENTE WHERE ID_CLIENTE = '" + idCliente + "';";
+        try {
+            ResultSet rs = comando1.executeQuery(query);
+            while (rs.next()) {
+
+                // pega todos os atributos da pessoa e guarda na classe
+                temp.setIdcliente(rs.getInt("ID_CLIENTE"));
+                temp.setNome(rs.getString("NOME_CLIENTE"));
+                temp.setCpf(rs.getString("CPF"));
+                temp.setRg(rs.getString("RG"));
+                temp.setTelefone(rs.getString("TELEFONE"));
+                temp.setCelular(rs.getString("CELULAR"));
+
+                // exibe os cadastros no banco
+                System.out.println(rs.getInt("ID_CLIENTE") + " | " + rs.getString("NOME_CLIENTE") + " | " + rs.getString("CPF") + " | " + rs.getString("RG") + " | " + rs.getString("TELEFONE") + " | " + rs.getString("CELULAR"));
+
+            }
+
+        } catch (SQLException e) {
+            Conexao.imprimeErro("Erro ao buscar cliente", e.getMessage());
+        }
+        return temp;
     }
 
     public void insere(cliente cliente) throws SQLException {
@@ -107,7 +152,7 @@ public class clienteDao {
                 sentenca.setString(6, cliente.getCelular());
                 sentenca.execute(); //executa o comando no banco
                 sentenca.close(); //fecha a sentenÃ§a
-               this.comando.prepareStatement(sql).close(); //fecha a conexÃ£o com o banco
+
             }
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
